857f26d3a1767b1b5cfebbab486b45574e3e6cb7
naranjito72 <theclick36@gmail.com>
2021-09-30 13:16:29 +0200

    second server

---
+++ server.js
@@ -0,0 +1,22 @@
+import http from 'http';
+import { Transform } from 'stream';
+
+const server = http.createServer();
+
+// recordr que http es una instancia de EVENTEMITTER
+// con lo cual podemos utilizr los métodos de EventEmitter (a repasar)
+const pongStr = new Transform({
+    transform(chunk, encoding, cb){
+        
+        if(chunk.toString().includes('PING'))
+        this.push('PONG');
+        cb();
+    }
+})
+server.on('request',( request, response) =>{
+    if(request.method === 'GET') response.end('Hello Me');
+    response.statusCode=418;
+    if(request.method === 'POST') request.pipe(pongStr).pipe(response);
+}).listen(8000);
+
+console.log('Listening in 8000');
